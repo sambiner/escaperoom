99bf6a5fc99235da79270a5fe1e7abcaffb5828d
sambiner <37849234+sambiner@users.noreply.github.com>
2019-02-22 14:22:47 -0800

    Add files via upload

---
+++ esc_rm.js
@@ -0,0 +1,127 @@
+function changeimg(el){
+	if (el.src.match("images/alien_x.png")){
+		el.src = "images/big_chill.png";
+	} else if (el.src.match("images/big_chill.png")){
+		el.src = "images/brainstorm.png";
+	} else if (el.src.match("images/brainstorm.png")){
+		el.src = "images/cannonbolt.png";
+	} else if (el.src.match("images/cannonbolt.png")){
+		el.src = "images/chromastone.png";
+	} else if (el.src.match("images/chromastone.png")){
+		el.src = "images/diamondhead.png";
+	} else if (el.src.match("images/diamondhead.png")){
+		el.src = "images/echo_echo.png";
+	} else if (el.src.match("images/echo_echo")){
+		el.src = "images/ghostfreak.png";
+	} else if (el.src.match("images/ghostfreak.png")){
+		el.src = "images/goop.png";
+	} else if (el.src.match("images/goop.png")){
+		el.src = "images/humungosaur.png";
+	} else if (el.src.match("images/humungosaur.png")){
+		el.src = "images/jetray.png";
+	} else if (el.src.match("images/jetray.png")){
+		el.src = "images/lodestar.png";
+	} else if (el.src.match("images/lodestar.png")){
+		el.src = "images/nanomech.png";
+	} else if (el.src.match("images/nanomech.png")){
+		el.src = "images/rath.png";
+	} else if (el.src.match("images/rath.png")){
+		el.src = "images/spider_monkey.png";
+	} else if (el.src.match("images/spider_monkey.png")){
+		el.src = "images/swampfire.png";
+	} else if (el.src.match("images/swampfire.png")){
+		el.src = "images/upchuck.png";
+	} else if (el.src.match("images/upchuck.png")){
+		el.src = "images/way_big.png";
+	} else if (el.src.match("images/way_big.png")){
+		el.src = "images/alien_x.png";
+	} else {
+		//do nothing
+	}
+	checkConfiguration();
+}
+function changeimg1(el){
+	if (el.src.match("images/alien_x.png")){
+		el.src = "images/big_chill.png";
+	} else if (el.src.match("images/big_chill.png")){
+		el.src = "images/brainstorm.png";
+	} else if (el.src.match("images/brainstorm.png")){
+		el.src = "images/cannonbolt.png";
+	} else if (el.src.match("images/cannonbolt.png")){
+		el.src = "images/chromastone.png";
+	} else if (el.src.match("images/chromastone.png")){
+		el.src = "images/diamondhead.png";
+	} else if (el.src.match("images/diamondhead.png")){
+		el.src = "images/echo_echo.png";
+	} else if (el.src.match("images/echo_echo")){
+		el.src = "images/ghostfreak.png";
+	} else if (el.src.match("images/ghostfreak.png")){
+		el.src = "images/goop.png";
+	} else if (el.src.match("images/goop.png")){
+		el.src = "images/humungosaur.png";
+	} else if (el.src.match("images/humungosaur.png")){
+		el.src = "images/jetray.png";
+	} else if (el.src.match("images/jetray.png")){
+		el.src = "images/lodestar.png";
+	} else if (el.src.match("images/lodestar.png")){
+		el.src = "images/nanomech.png";
+	} else if (el.src.match("images/nanomech.png")){
+		el.src = "images/rath.png";
+	} else if (el.src.match("images/rath.png")){
+		el.src = "images/spider_monkey.png";
+	} else if (el.src.match("images/spider_monkey.png")){
+		el.src = "images/swampfire.png";
+	} else if (el.src.match("images/swampfire.png")){
+		el.src = "images/upchuck.png";
+	} else if (el.src.match("images/upchuck.png")){
+		el.src = "images/way_big.png";
+	} else if (el.src.match("images/way_big.png")){
+		el.src = "images/alien_x.png";
+	} else {
+		//do nothing
+	}
+	checkConfiguration();
+}
+function changeimg2(el){
+	if (el.src.match("images/alien_x.png")){
+		el.src = "images/big_chill.png";
+	} else if (el.src.match("images/big_chill.png")){
+		el.src = "images/brainstorm.png";
+	} else if (el.src.match("images/brainstorm.png")){
+		el.src = "images/cannonbolt.png";
+	} else if (el.src.match("images/cannonbolt.png")){
+		el.src = "images/chromastone.png";
+	} else if (el.src.match("images/chromastone.png")){
+		el.src = "images/diamondhead.png";
+	} else if (el.src.match("images/diamondhead.png")){
+		el.src = "images/echo_echo.png";
+	} else if (el.src.match("images/echo_echo")){
+		el.src = "images/ghostfreak.png";
+	} else if (el.src.match("images/ghostfreak.png")){
+		el.src = "images/goop.png";
+	} else if (el.src.match("images/goop.png")){
+		el.src = "images/humungosaur.png";
+	} else if (el.src.match("images/humungosaur.png")){
+		el.src = "images/jetray.png";
+	} else if (el.src.match("images/jetray.png")){
+		el.src = "images/lodestar.png";
+	} else if (el.src.match("images/lodestar.png")){
+		el.src = "images/nanomech.png";
+	} else if (el.src.match("images/nanomech.png")){
+		el.src = "images/rath.png";
+	} else if (el.src.match("images/rath.png")){
+		el.src = "images/spider_monkey.png";
+	} else if (el.src.match("images/spider_monkey.png")){
+		el.src = "images/swampfire.png";
+	} else if (el.src.match("images/swampfire.png")){
+		el.src = "images/upchuck.png";
+	} else if (el.src.match("images/upchuck.png")){
+		el.src = "images/way_big.png";
+	} else if (el.src.match("images/way_big.png")){
+		el.src = "images/alien_x.png";
+	} else {
+		//do nothing
+	}
+	checkConfiguration();
+}
+
