7a59183e5257ea7bf84512cb90537bf38a4dd9ca
sambiner <37849234+sambiner@users.noreply.github.com>
2019-03-03 19:39:58 -0800

    Add files via upload

--- esc_rm.js
+++ esc_rm.js
@@ -2,83 +2,119 @@ var click_counter = 0;
 var audio = document.getElementById("audio");
 
 function changeimg(el){ //This makes the images cycle for the 1st image
-	if (el.src.match("images/alien_x.png")){
+	if (el.src.match("images/alien_x.png") && document.getElementById("placeholder").innerHTML == "Alien X"){
 		el.src = "images/big_chill.png";
-	} else if (el.src.match("images/big_chill.png")){
+		document.getElementById("placeholder").innerHTML = "Big Chill";
+	} else if (el.src.match("images/big_chill.png") && document.getElementById("placeholder").innerHTML == "Big Chill"){
 		el.src = "images/brainstorm.png";
-	} else if (el.src.match("images/brainstorm.png")){
+		document.getElementById("placeholder").innerHTML = "Brainstorm";
+	} else if (el.src.match("images/brainstorm.png") && document.getElementById("placeholder").innerHTML == "Brainstorm"){
 		el.src = "images/cannonbolt.png";
-	} else if (el.src.match("images/cannonbolt.png")){
+		document.getElementById("placeholder").innerHTML = "Cannonbolt";
+	} else if (el.src.match("images/cannonbolt.png") && document.getElementById("placeholder").innerHTML == "Cannonbolt"){
 		el.src = "images/chromastone.png";
-	} else if (el.src.match("images/chromastone.png")){
+		document.getElementById("placeholder").innerHTML = "Chromastone";
+	} else if (el.src.match("images/chromastone.png") && document.getElementById("placeholder").innerHTML == "Chromastone"){
 		el.src = "images/diamondhead.png";
-	} else if (el.src.match("images/diamondhead.png")){
+		document.getElementById("placeholder").innerHTML = "Diamondhead";
+	} else if (el.src.match("images/diamondhead.png") && document.getElementById("placeholder").innerHTML == "Diamondhead"){
 		el.src = "images/echo_echo.png";
-	} else if (el.src.match("images/echo_echo")){
+		document.getElementById("placeholder").innerHTML = "Echo Echo";
+	} else if (el.src.match("images/echo_echo") && document.getElementById("placeholder").innerHTML == "Echo Echo"){
 		el.src = "images/ghostfreak.png";
-	} else if (el.src.match("images/ghostfreak.png")){
+		document.getElementById("placeholder").innerHTML = "Ghostfreak";
+	} else if (el.src.match("images/ghostfreak.png") && document.getElementById("placeholder").innerHTML == "Ghostfreak"){
 		el.src = "images/goop.png";
-	} else if (el.src.match("images/goop.png")){
+		document.getElementById("placeholder").innerHTML = "Goop";
+	} else if (el.src.match("images/goop.png") && document.getElementById("placeholder").innerHTML == "Goop"){
 		el.src = "images/humungosaur.png";
-	} else if (el.src.match("images/humungosaur.png")){
+		document.getElementById("placeholder").innerHTML = "Humungosaur";
+	} else if (el.src.match("images/humungosaur.png") && document.getElementById("placeholder").innerHTML == "Humungosaur"){
 		el.src = "images/jetray.png";
-	} else if (el.src.match("images/jetray.png")){
+		document.getElementById("placeholder").innerHTML = "Jetray";
+	} else if (el.src.match("images/jetray.png") && document.getElementById("placeholder").innerHTML == "Jetray"){
 		el.src = "images/lodestar.png";
-	} else if (el.src.match("images/lodestar.png")){
+		document.getElementById("placeholder").innerHTML = "Lodestar";
+	} else if (el.src.match("images/lodestar.png") && document.getElementById("placeholder").innerHTML == "Lodestar"){
 		el.src = "images/nanomech.png";
-	} else if (el.src.match("images/nanomech.png")){
+		document.getElementById("placeholder").innerHTML = "Nanomech";
+	} else if (el.src.match("images/nanomech.png") && document.getElementById("placeholder").innerHTML == "Nanomech"){
 		el.src = "images/rath.png";
-	} else if (el.src.match("images/rath.png")){
+		document.getElementById("placeholder").innerHTML = "Rath";
+	} else if (el.src.match("images/rath.png") && document.getElementById("placeholder").innerHTML == "Rath"){
 		el.src = "images/spider_monkey.png";
-	} else if (el.src.match("images/spider_monkey.png")){
+		document.getElementById("placeholder").innerHTML = "Spider Monkey";
+	} else if (el.src.match("images/spider_monkey.png") && document.getElementById("placeholder").innerHTML == "Spider Monkey"){
 		el.src = "images/swampfire.png";
-	} else if (el.src.match("images/swampfire.png")){
+		document.getElementById("placeholder").innerHTML = "Swampfire";
+	} else if (el.src.match("images/swampfire.png") && document.getElementById("placeholder").innerHTML == "Swampfire"){
 		el.src = "images/upchuck.png";
-	} else if (el.src.match("images/upchuck.png")){
+		document.getElementById("placeholder").innerHTML = "Upchuck";
+	} else if (el.src.match("images/upchuck.png") && document.getElementById("placeholder").innerHTML == "Upchuck"){
 		el.src = "images/way_big.png";
-	} else if (el.src.match("images/way_big.png")){
+		document.getElementById("placeholder").innerHTML = "Way Big";
+	} else if (el.src.match("images/way_big.png") && document.getElementById("placeholder").innerHTML == "Way Big"){
 		el.src = "images/alien_x.png";
+		document.getElementById("placeholder").innerHTML = "Alien X";
 	} else {
 		//do nothing
 	}
 }
 function changeimg1(el){ //This makes the images cycle for the 2nd image
-	if (el.src.match("images/alien_x.png")){
+	if (el.src.match("images/alien_x.png") && document.getElementById("placeholder1").innerHTML == "Alien X"){
 		el.src = "images/big_chill.png";
-	} else if (el.src.match("images/big_chill.png")){
+		document.getElementById("placeholder1").innerHTML = "Big Chill";
+	} else if (el.src.match("images/big_chill.png") && document.getElementById("placeholder1").innerHTML == "Big Chill"){
 		el.src = "images/brainstorm.png";
-	} else if (el.src.match("images/brainstorm.png")){
+		document.getElementById("placeholder1").innerHTML = "Brainstorm";
+	} else if (el.src.match("images/brainstorm.png") && document.getElementById("placeholder1").innerHTML == "Brainstorm"){
 		el.src = "images/cannonbolt.png";
-	} else if (el.src.match("images/cannonbolt.png")){
+		document.getElementById("placeholder1").innerHTML = "Cannonbolt";
+	} else if (el.src.match("images/cannonbolt.png") && document.getElementById("placeholder1").innerHTML == "Cannonbolt"){
 		el.src = "images/chromastone.png";
-	} else if (el.src.match("images/chromastone.png")){
+		document.getElementById("placeholder1").innerHTML = "Chromastone";
+	} else if (el.src.match("images/chromastone.png") && document.getElementById("placeholder1").innerHTML == "Chromastone"){
 		el.src = "images/diamondhead.png";
-	} else if (el.src.match("images/diamondhead.png")){
+		document.getElementById("placeholder1").innerHTML = "Diamondhead";
+	} else if (el.src.match("images/diamondhead.png") && document.getElementById("placeholder1").innerHTML == "Diamondhead"){
 		el.src = "images/echo_echo.png";
-	} else if (el.src.match("images/echo_echo")){
+		document.getElementById("placeholder1").innerHTML = "Echo Echo";
+	} else if (el.src.match("images/echo_echo") && document.getElementById("placeholder1").innerHTML == "Echo Echo"){
 		el.src = "images/ghostfreak.png";
-	} else if (el.src.match("images/ghostfreak.png")){
+		document.getElementById("placeholder1").innerHTML = "Ghostfreak";
+	} else if (el.src.match("images/ghostfreak.png") && document.getElementById("placeholder1").innerHTML == "Ghostfreak"){
 		el.src = "images/goop.png";
-	} else if (el.src.match("images/goop.png")){
+		document.getElementById("placeholder1").innerHTML = "Goop";
+	} else if (el.src.match("images/goop.png") && document.getElementById("placeholder1").innerHTML == "Goop"){
 		el.src = "images/humungosaur.png";
-	} else if (el.src.match("images/humungosaur.png")){
+		document.getElementById("placeholder1").innerHTML = "Humungosaur";
+	} else if (el.src.match("images/humungosaur.png") && document.getElementById("placeholder1").innerHTML == "Humungosaur"){
 		el.src = "images/jetray.png";
-	} else if (el.src.match("images/jetray.png")){
+		document.getElementById("placeholder1").innerHTML = "Jetray";
+	} else if (el.src.match("images/jetray.png") && document.getElementById("placeholder1").innerHTML == "Jetray"){
 		el.src = "images/lodestar.png";
-	} else if (el.src.match("images/lodestar.png")){
+		document.getElementById("placeholder1").innerHTML = "Lodestar";
+	} else if (el.src.match("images/lodestar.png") && document.getElementById("placeholder1").innerHTML == "Lodestar"){
 		el.src = "images/nanomech.png";
-	} else if (el.src.match("images/nanomech.png")){
+		document.getElementById("placeholder1").innerHTML = "Nanomech";
+	} else if (el.src.match("images/nanomech.png") && document.getElementById("placeholder1").innerHTML == "Nanomech"){
 		el.src = "images/rath.png";
-	} else if (el.src.match("images/rath.png")){
+		document.getElementById("placeholder1").innerHTML = "Rath";
+	} else if (el.src.match("images/rath.png") && document.getElementById("placeholder1").innerHTML == "Rath"){
 		el.src = "images/spider_monkey.png";
-	} else if (el.src.match("images/spider_monkey.png")){
+		document.getElementById("placeholder1").innerHTML = "Spider Monkey";
+	} else if (el.src.match("images/spider_monkey.png") && document.getElementById("placeholder1").innerHTML == "Spider Monkey"){
 		el.src = "images/swampfire.png";
-	} else if (el.src.match("images/swampfire.png")){
+		document.getElementById("placeholder1").innerHTML = "Swampfire";
+	} else if (el.src.match("images/swampfire.png") && document.getElementById("placeholder1").innerHTML == "Swampfire"){
 		el.src = "images/upchuck.png";
-	} else if (el.src.match("images/upchuck.png")){
+		document.getElementById("placeholder1").innerHTML = "Upchuck";
+	} else if (el.src.match("images/upchuck.png") && document.getElementById("placeholder1").innerHTML == "Upchuck"){
 		el.src = "images/way_big.png";
-	} else if (el.src.match("images/way_big.png")){
+		document.getElementById("placeholder1").innerHTML = "Way Big";
+	} else if (el.src.match("images/way_big.png") && document.getElementById("placeholder1").innerHTML == "Way Big"){
 		el.src = "images/alien_x.png";
+		document.getElementById("placeholder1").innerHTML = "Alien X";
 	} else {
 		//do nothing
 	}
@@ -173,4 +209,8 @@ function checkCounter(){
 	} else{
 		
 	}
+}
+
+function winGame(el){
+	
 }
